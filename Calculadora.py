87988c8c4f14083edc716f831e7c99e481616077
JoaquinTielas <joaquintielas@gmail.com>
2022-04-03 11:09:46 -0300

    Inicio de Calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -1,10 +1,7 @@
 import tkinter
-from turtle import bgcolor
-ventana = tkinter.Tk()
-ventana.geometry("500x400")
-Titulo = tkinter.Label(ventana,text="Calculadora", bg = "red")
-Titulo.pack(side= tkinter.BOTTOM, fill= tkinter.X)
+
+ventana = Tk()
+ventana.Title("Calculadora")
 
 
 ventana.mainloop()
-
